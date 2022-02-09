423b566ef38be484add19b1377a905c98711c6c6
MaxAvila <maxvilam@hotmail.con>
2022-02-08 23:49:36 -0300

    Ok

--- assets/jv/ej.js
+++ assets/jv/ej.js
@@ -1,21 +1,23 @@
 let b=true;
 let n=0;
-do{
-    n= prompt("favor ingrese un numero tabla de Multiplicar y factorial");
-    if (n>20 || n<1)
+let f=1;
+const ingresenumero=() => {
+    do{
+        n= prompt("favor ingrese un numero tabla de Multiplicar y factorial");
+        if (n>20 || n<1)
         {console.log("Numero fuera de rango, intente nuevamente")};
-    b= (n>20) || (n<1);
+        b= (n>20) || (n<1);
+    } while (b);
+    return n;
+} 
 
-} while (b);
+let n1=ingresenumero()
 
-const tmf =(n1) => {
-    for (let i = 1; i <= n1; i++) {
-        console.log(i+"x"+ n1+"="+i*n1);    
-    }
-    let f=1;
-    for (let i = 1; i <= n1; i++) {
-        f=i*f;
-        console.log("Factorial de", i, "es:", f);
-    }
+for (let i = 1; i <= n1; i++) {
+    console.log(i+"x"+ n1+"="+i*n1);    
 }
-tmf(n)
+
+for (let i = 1; i <= n1; i++) {
+    f=i*f;
+    console.log("Factorial de", i, "es:", f);
+}
