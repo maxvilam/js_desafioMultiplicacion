e904873e76abb2c6052df3731d231813c097d452
MaxAvila <maxvilam@hotmail.con>
2022-02-10 02:06:00 -0300

    End

--- assets/jv/ej.js
+++ assets/jv/ej.js
@@ -1,23 +1,24 @@
-let b=true;
-let n=0;
-let f=1;
-const ingresenumero=() => {
-    do{
-        n= prompt("favor ingrese un numero tabla de Multiplicar y factorial");
-        if (n>20 || n<1)
-        {console.log("Numero fuera de rango, intente nuevamente")};
-        b= (n>20) || (n<1);
-    } while (b);
-    return n;
-} 
+let b = true;
+let n = 0;
+let f = 1;
+const ingresenumero = () => {
+  do {
+    n = prompt("favor ingrese un numero tabla de Multiplicar y factorial");
+    if (n > 20 || n < 1) {
+      console.log("Numero fuera de rango, intente nuevamente");
+    }
+    b = n > 20 || n < 1;
+  } while (b);
+  return n;
+};
 
-let n1=ingresenumero()
+let n1 = ingresenumero();
 
 for (let i = 1; i <= n1; i++) {
-    console.log(i+"x"+ n1+"="+i*n1);    
+  console.log(i + "x" + n1 + "=" + i * n1);
 }
 
 for (let i = 1; i <= n1; i++) {
-    f=i*f;
-    console.log("Factorial de", i, "es:", f);
+  f = i * f;
+  console.log("Factorial de", i, "es:", f);
 }
